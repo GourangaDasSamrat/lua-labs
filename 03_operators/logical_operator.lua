9b7ddbc386000196d03950d2a2dd64f135ec7a14
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-04-07 10:21:15 +0600

    feat(operators): add logical operator examples
    - Added Lua script for logical operators
    - Demonstrates AND, OR, and NOT operations
    - Includes print statements for output

---
+++ 03_operators/logical_operator.lua
@@ -0,0 +1,11 @@
+local hasWonSlam = true
+local isRankOne = false
+
+-- Logical AND
+print(hasWonSlam and isRankOne) -- false
+
+-- Logical OR
+print(hasWonSlam or isRankOne) -- true
+
+-- Logical NOT
+print(not hasWonSlam) -- false
