8d55692b5caa14ddce6f232d3716396af04e4a33
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-04-07 10:19:53 +0600

    feat(operators): add relational operators example
    - Demonstrates equality (==, ~=)
    - Demonstrates inequality (<, >, <=, >=)

---
+++ 03_operators/relational_operators.lua
@@ -0,0 +1,7 @@
+local arynaRank = 1
+local rybakinaRank = 4
+
+print(arynaRank == rybakinaRank) -- false (Equality)
+print(arynaRank ~= rybakinaRank) -- true  (Not Equal)
+print(arynaRank < rybakinaRank)  -- true  (Less than)
+print(arynaRank >= 1)            -- true  (Greater than or  equal)
