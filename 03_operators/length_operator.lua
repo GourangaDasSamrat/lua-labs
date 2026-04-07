b7969749489272f54497d31270eef08242fab61b
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-04-07 10:24:33 +0600

    feat(operators): add length operator example
    - Demonstrates # operator on strings
    - Demonstrates # operator on tables

---
+++ 03_operators/length_operator.lua
@@ -0,0 +1,5 @@
+local player = "Rybakina"
+print(#player) -- Output: 8 (Length of string)
+
+local players = { "Aryna", "Rybakina", "Magnus" }
+print(#players) -- Output: 3 (Number of elements in array-like table)
