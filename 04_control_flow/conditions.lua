cda69a03ca770a18b309d9a89076286c30398006
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-04-09 14:43:27 +0600

    feat(control_flow): add conditions.lua example
    - Implemented basic if-elseif-else
    - Demonstrates conditional logic
    - Uses player and rank variables

---
+++ 04_control_flow/conditions.lua
@@ -0,0 +1,11 @@
+local player = "Sabalenka"
+local rank = 1
+
+-- Basic if-elseif-else structure
+if rank == 1 and player == "Sabalenka" then
+    print(player .. " is at the top!") -- Output: Sabalenka is at the top!
+elseif rank == 2 then
+    print("Rybakina is chasing the spot.")
+else
+    print("Other top players.")
+end
