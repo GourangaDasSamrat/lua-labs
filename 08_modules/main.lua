0530e5dce2edfa7b878ad8d138c1628b72ba815d
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-04-10 17:05:54 +0600

    feat(modules): implement math utility module
    - Created a new module for math utilities
    - Added a function to add two numbers
    - Included a player name constant

---
+++ 08_modules/main.lua
@@ -0,0 +1,4 @@
+local mathUtils = require("math_utils") -- Importing
+
+print(mathUtils.playerName)             -- Rybakina
+print(mathUtils.addPoints(10, 20))      -- 30
---
+++ 08_modules/math_utils.lua
@@ -0,0 +1,9 @@
+local M = {} -- Module table
+
+function M.addPoints(a, b)
+    return a + b
+end
+
+M.playerName = "Rybakina"
+
+return M -- Exporting the table
