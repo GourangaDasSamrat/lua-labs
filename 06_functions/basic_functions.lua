396e0df2d663181132dd044dca054774630e43d6
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-04-10 16:54:59 +0600

    feat(functions): add basic Lua function examples
    - Implemented simple function declaration
    - Added function calling example
    - Showed storing function in a variable

---
+++ 06_functions/basic_functions.lua
@@ -0,0 +1,14 @@
+-- Simple function declaration
+local function greetSabalenka()
+    print("Hello, Aryna Sabalenka!")
+end
+
+-- Calling the function
+greetSabalenka()
+
+-- Another way (storing function in a variable)
+local congratulate = function()
+    print("Congratulations on the win, Rybakina!")
+end
+
+congratulate()
